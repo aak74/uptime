e6a4417ee0db093040c4abc97c509775009f0aea
Andrew Kopylov <riu@inbox.ru>
2018-04-20 00:08:09 +0300

    refactor to channel

--- pinger-go/main.go
+++ pinger-go/main.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sync"
 	"time"
 )
 
@@ -39,36 +38,70 @@ func getUrls() (sites []site, err error) {
 }
 
 func main() {
-	var wg sync.WaitGroup
 	sites, err := getUrls()
+
 	if err != nil {
 		fmt.Println("Error", err)
 		os.Exit(1)
 	}
 	start := time.Now()
+	msg := make(chan string, 10)
+	done := make(chan bool)
+	// until := time.After(5 * time.Second)
+
+	// var wg sync.WaitGroup
+	// pinger := make(chan string)
 	for i, url := range sites {
-		wg.Add(1)
-		go func(url string, i int) {
-			start := time.Now()
-			resp, _ := http.Get(url)
-			secs := time.Since(start).Seconds()
-			if resp == nil {
-				fmt.Println(fmt.Sprintf("%d task fail after %.2fs: %s", i, secs, url))
-			} else {
-				body, _ := ioutil.ReadAll(resp.Body)
-				fmt.Println(fmt.Sprintf("%d task %.2fs elapsed with response length: %d %s", i, secs, len(body), url))
-			}
-			wg.Done()
-		}(url.URL, i)
-		wg.Wait()
+		// wg.Add(1)
+		go request(msg, done, url.URL, i)
+		// wg.Wait()
 	}
 
-	// for i := range sites {
-	// 	res, _ := <-ch
-	// 	fmt.Println(fmt.Printf("%d/%d - %s", i+1, len(sites), res))
-	// }
-	// close(ch)
-
+	for i := range sites {
+		res, _ := <-msg
+		fmt.Println(fmt.Printf("%d/%d - %s", i+1, len(sites), res))
+	}
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+	close(msg)
 
+	// for i := range sites {
+	// i := 0
+	// for {
+	// 	i++
+	// 	fmt.Println(i)
+	// 	select {
+	// 	case m := <-msg:
+	// 		fmt.Println(m)
+	// 	case <-until:
+	// 		done <- true
+	// 		time.Sleep(500 * time.Millisecond)
+	// 		fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+	// 		return
+	// 	}
+	// }
+	// }
+
+}
+
+func request(ch chan<- string, done <-chan bool, url string, i int) {
+	fmt.Println(i, url)
+	// for {
+	// 	select {
+	// 	case <-done:
+	// 		println("Done")
+	// 		close(ch)
+	// 		return
+	// 	default:
+	start := time.Now()
+	resp, _ := http.Get(url)
+	secs := time.Since(start).Seconds()
+	if resp == nil {
+		ch <- fmt.Sprintf("%d task fail after %.2fs: %s", i, secs, url)
+	} else {
+		body, _ := ioutil.ReadAll(resp.Body)
+		ch <- fmt.Sprintf("%d task %.2fs elapsed with response length: %d %s", i, secs, len(body), url)
+	}
+	// wg.Done()
+	// 	}
+	// }
 }
