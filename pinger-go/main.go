f2e3dc98a582e068c94726cd1d9217eef20aaf75
Andrew Kopylov <riu@inbox.ru>
2018-04-19 23:29:18 +0300

    minor refactor

--- pinger-go/main.go
+++ pinger-go/main.go
@@ -6,10 +6,12 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"sync"
 	"time"
 )
 
-type Site struct {
+type site struct {
 	ID string `json:"id"`
 	// Domain string `json:"domain"`
 	URL string `json:"url"`
@@ -17,7 +19,7 @@ type Site struct {
 	// Timeout   int    `json:"timeout"`
 }
 
-func GetUrls() (sites []Site, err error) {
+func getUrls() (sites []site, err error) {
 	resp, err := http.Get("http://127.0.0.1/api/v1/projects")
 	// fmt.Println("resp:", err)
 	if err == nil {
@@ -36,38 +38,37 @@ func GetUrls() (sites []Site, err error) {
 	return
 }
 
-func MakeRequest(url string, ch chan<- string, i int) {
-	start := time.Now()
-	resp, _ := http.Get(url)
-	secs := time.Since(start).Seconds()
-	if resp == nil {
-		ch <- fmt.Sprintf("%d task fail after %.2fs: %s", i, secs, url)
-	} else {
-		body, _ := ioutil.ReadAll(resp.Body)
-		ch <- fmt.Sprintf("%d task %.2fs elapsed with response length: %d %s", i, secs, len(body), url)
-	}
-}
-
 func main() {
-	var sites, err = GetUrls()
-	// fmt.Print(sites, err)
-	if err == nil {
-		// fmt.Print(sites)
-		// return fmt.Print(urls)
-		start := time.Now()
-		ch := make(chan string, 20)
-		for i, url := range sites {
-			go MakeRequest(url.URL, ch, i)
-		}
-
-		for i := range sites {
-			res, _ := <-ch
-			fmt.Println(fmt.Printf("%d/%d - %s", i+1, len(sites), res))
-		}
-		close(ch)
-
-		fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
-
+	var wg sync.WaitGroup
+	sites, err := getUrls()
+	if err != nil {
+		fmt.Println("Error", err)
+		os.Exit(1)
 	}
+	start := time.Now()
+	for i, url := range sites {
+		wg.Add(1)
+		go func(url string, i int) {
+			start := time.Now()
+			resp, _ := http.Get(url)
+			secs := time.Since(start).Seconds()
+			if resp == nil {
+				fmt.Println(fmt.Sprintf("%d task fail after %.2fs: %s", i, secs, url))
+			} else {
+				body, _ := ioutil.ReadAll(resp.Body)
+				fmt.Println(fmt.Sprintf("%d task %.2fs elapsed with response length: %d %s", i, secs, len(body), url))
+			}
+			wg.Done()
+		}(url.URL, i)
+		wg.Wait()
+	}
+
+	// for i := range sites {
+	// 	res, _ := <-ch
+	// 	fmt.Println(fmt.Printf("%d/%d - %s", i+1, len(sites), res))
+	// }
+	// close(ch)
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 
 }
